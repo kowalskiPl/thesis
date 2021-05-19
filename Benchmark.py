2eb11843535bcb63d6674b7fe8e7ad68cfba80b6
Patryk Kobus <patf117@gmail.com>
2021-05-19 14:57:27 +0200

    Added keypoints drawing, changes to color conversion

--- Benchmark.py
+++ Benchmark.py
@@ -4,9 +4,10 @@ import time
 from enum import Enum
 
 import cv2
+import imutils
 import numpy as np
 
-orb_detector = cv2.cuda_ORB.create(nfeatures=300)
+orb_detector = cv2.cuda_ORB.create(nfeatures=500)
 fast_detector = cv2.cuda_FastFeatureDetector.create()
 
 
@@ -22,13 +23,15 @@ def detect_features(image: np.ndarray, detector: Detector):
     global orb_detector
     global fast_detector
     global cuda_mat
-    grey_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    cuda_mat.upload(grey_img)
+    cuda_mat.upload(image)
+    grey_mat = cv2.cuda.cvtColor(cuda_mat, cv2.COLOR_RGB2GRAY)
 
     if detector is Detector.ORB:
-        kp, ds = orb_detector.detectAndComputeAsync(cuda_mat, None)
+        kp, ds = orb_detector.detectAndComputeAsync(grey_mat, None)
+        return kp, ds
     else:
-        kp, ds = fast_detector.detectAndComputeAsync(cuda_mat, None)
+        kp, ds = fast_detector.detectAndComputeAsync(grey_mat, None)
+        return kp, ds
 
 
 def draw_keypoints_orb(kp, image):
@@ -43,6 +46,8 @@ def draw_keypoints_fast(kp, image):
 
 def main():
     global detector_type
+    global fast_detector
+    global orb_detector
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--detect-compute", action="store_true", required=False)
     ap.add_argument("-s", "--stitch", action="store_true", required=False)
@@ -51,6 +56,7 @@ def main():
     ap.add_argument("-o", "--orb", action="store_true", required=False)
     ap.add_argument("-f", "--fast", action="store_true", required=False)
     ap.add_argument("-it", "--iterations", type=int, default=10, required=False)
+    ap.add_argument("-dk", "--draw-keypoints", action="store_true", required=False)
     args = vars(ap.parse_args())
 
     if args["stitch"] is True and args["image_2"] is None:
@@ -61,9 +67,11 @@ def main():
         sys.exit("Provide one detector type!")
 
     image_1 = cv2.imread("benchmarkImages/" + args["image_1"])
+    image_1 = imutils.resize(image_1, width=1920)
     image_2: np.ndarray
     if args["image_2"] is not None:
         image_2 = cv2.imread("benchmarkImages/" + args["image_2"])
+        image_2 = imutils.resize(image_2, width=1920)
 
     if args["orb"] is True:
         detector_type = Detector.ORB
@@ -72,12 +80,18 @@ def main():
 
     iterations = args["iterations"]
     if args["detect_compute"] is True:
+        kp = None
+        ds = None
         start_time = time.perf_counter()
         for i in range(iterations):
-            detect_features(image_1, detector_type)
+            kp, ds = detect_features(image_1, detector_type)
         end_time = time.perf_counter()
         print("Average detection time using: " + detector_type.name + " with " + str(iterations) + " iterations:")
         print((end_time - start_time) / args["iterations"])
+        if args["draw_keypoints"] is True:
+            if detector_type is Detector.ORB:
+                keypoints_frame = cv2.drawKeypoints(image_1, orb_detector.convert(kp), None, color=(255, 0, 50))
+                cv2.imwrite(args["image_1"] + "_result.png", keypoints_frame)
 
 
 if __name__ == '__main__':
